32a18d9335ed3196311d2663efffc6c53a813acf
Martins Savickis <morowinder@gmail.com>
2018-06-18 16:43:20 +0300

    refactor estimate query building, catch invalid API responses

---
+++ src/Geta.Bring.EPi.Commerce/Extensions/BringCommerceRegistryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Geta.Bring.EPi.Commerce.Factories;
+using Mediachase.Commerce.Orders;
+using StructureMap;
+
+namespace Geta.Bring.EPi.Commerce.Extensions
+{
+    public static class BringCommerceRegistryExtensions
+    {
+        public static T RegisterBringContainers<T>(this T registry) where T : Registry
+        {
+            registry.For<IShippingPlugin>().Transient().Use<BringShippingGateway>();
+            registry.For<IEstimateQueryFactory>().Transient().Use<EstimateQueryFactory>();
+            return registry;
+        }
+    }
+}
---
+++ src/Geta.Bring/Shipping/ShippingErrorCodes.cs
@@ -0,0 +1,9 @@
+﻿namespace Geta.Bring.Shipping
+{
+    public static class ShippingErrorCodes
+    {
+        public const string MeasurementsExceeded = "FG_MEASUREMENTS_EXCEEDED";
+
+        public const string Unknown = "FG_UNKNOWN";
+    }
+}
