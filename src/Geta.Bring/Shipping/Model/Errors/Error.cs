ec104c07ee0d9515b312f800fd51fe20a704cfd7
Sven-Erik Jonsson <sven.erik@geta.no>
2019-01-17 10:15:50 +0100

    Adjusted model to allow for errors in ProductResponse.

--- src/Geta.Bring/Shipping/Model/Errors/Error.cs
+++ src/Geta.Bring/Shipping/Model/Errors/Error.cs
@@ -1,7 +1,14 @@
-﻿namespace Geta.Bring.Shipping.Model.Errors
+﻿using Newtonsoft.Json;
+
+namespace Geta.Bring.Shipping.Model.Errors
 {
     public class Error
     {
+        public Error(string code)
+        {
+            Code = code;
+        }
+
         public string Code { get; set; }
         public override string ToString()
         {
--- src/Tests.Integration/Shipping/ShippingClientTests.cs
+++ src/Tests.Integration/Shipping/ShippingClientTests.cs
@@ -78,7 +78,7 @@ namespace Tests.Integration.Shipping
             var sut = new ShippingClient(settings);
 
             var query = new EstimateQuery(
-                new ShipmentLeg("0000", "5600", "NO", "NO"),
+                new ShipmentLeg("0000", "5600"),
                 PackageSize.InGrams(2500),
                 new Products(Product.Servicepakke));
 
@@ -88,6 +88,29 @@ namespace Tests.Integration.Shipping
                                            .Where(x => x.Code.Equals("INVALID_ARGUMENT"));
 
             Assert.Single(fieldErrors);
+        }
+
+        [Fact]
+        public async Task it_does_not_return_product_because_weight_is_over_limit()
+        {
+            var clientUri = new Uri("http://test.localtest.me");
+
+            var settings = new ShippingSettings(clientUri, "sven.erik@geta.no", "20b22ed6-2aa7-48c9-9561-b49fe85ce118");
+            var sut = new ShippingClient(settings);
+
+            var query = new EstimateQuery(
+                new ShipmentLeg("0151", "5003"),
+                PackageSize.InGrams(300000),
+                new Products(Product.Servicepakke));
+
+            var actual = await sut.FindAsync<ShipmentEstimate>(query).ConfigureAwait(false);
+
+            var productCount = actual.Estimates.Count();
+            var productErrors = actual.Errors.OfType<ProductError>()
+                                      .Where(x => x.Code.Equals("INVALID_MEASUREMENTS"));
+
+            Assert.Equal(0, productCount);
+            Assert.Single(productErrors);
         }
     }
 }
